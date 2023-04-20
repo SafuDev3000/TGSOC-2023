eaa20ee69fe60fcc782cf56413e4e16d615d376f
SafuDev3000 <genograndino@gmail.com>
2023-04-20 00:43:17 -0700

    fixed

---
+++ js/custom.js
@@ -0,0 +1,103 @@
+// ------- PRELOADER -------//
+$(window).load(function(){
+    $('.preloader').fadeOut("slow"); // set duration in brackets    
+});
+// ----- GOOGLE MAP ----- //
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(function(){
+
+   // --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+  // --------- PORTFOLIO IMAGE ----- //
+  $('#portfolio a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+  // ------- WOW ANIMATED ------ //
+  wow = new WOW(
+  {
+    mobile: false
+  });
+  wow.init();
+
+  // ------- GOOGLE MAP ----- //
+  loadGoogleMap();
+
+  // ------- JQUERY PARALLAX ---- //
+  function initParallax() {
+    $('#home').parallax("100%", 0.3);
+    $('#team').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.1);
+
+  }
+  initParallax();
+
+});
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}

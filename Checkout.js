d53f9410a6a6f6c6e9a98152515901d4c79d6090
SafuDev3000 <genograndino@gmail.com>
2023-12-25 21:56:00 -0800

    update footer

--- Checkout.js
+++ Checkout.js
@@ -12,9 +12,9 @@ var btn = document.getElementById("purchaseBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
